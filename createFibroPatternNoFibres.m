631d04e2a548e54b9e78e869f298c5cd704f4f2d
Beta Lawson <brodie.lawson86@gmail.com>
2018-12-10 15:56:42 +1000

    Add support for matchHistology runs without fibres
    Updated creation of fibropatterns with no fibre
    parameters supplied, and a new runner for this
    purpose, matchHistologyNoFibres.m

--- createFibroPatternNoFibres.m
+++ createFibroPatternNoFibres.m
@@ -1,22 +1,28 @@
-function [presence, P_f, P_p] = createFibroPatternSeededNoFibres(mesh, density, params, P, offsets)
+function [presence, P_f, P_p] = createFibroPatternNoFibres(mesh, density, params, Ps, offsets)
 
 % This function takes a list of points and a set of parameters (listed 
 % below), and creates a pattern of fibrosis accordingly.
 %
-% Usage:    presence = CreateFibroPatternNew(points, direction, density, params, seed)
+% Usage:    presence = CreateFibroPattern(points, direction, density, params, Ps, offsets)
 %
 % INPUTS:   mesh:      information that defines the mesh over which to create a pattern
 %           density:   the density of fibrosis
 %           params:    a set of parameters defining the noise pattern (see below)
+%           Ps:        an m x n matrix, which is m rows of the numbers 0:n-1 arranged in random order (permutation tables for random assignment of vectors in Perlin noise)
+%           offsets:   an m x 2 matrix that specifies grid offsets for each octave in octave noise
+%
+%           ( m is the maximum number of octaves that will be requested by
+%           this function, and n is the number of 
 %
 % OUTPUTS:  presence:  a presence/absence map of fibrosis
+%           (S):       the sinusoidal field used for fibre-type patterns (optional)
 %           (Pf):      the Perlin noise field for fibrosis (optional)
 %           (Pp):      the Perlin noise field for density variation (optional)
 %
 % PARAMS:   Parameters are provided as a single vector, for ease of
 %           interface with other code.
 %           Params vector is specified as:
-%           [ patchiness, feature_size, roughness, patch_size, alignment_ratio, direction]
+%           [ fibreness, fibre_separation, patchiness, feature_size, roughness, patch_size, alignment_ratio, direction]
 
 % Read out paramaters from vector
 params = num2cell(params);
@@ -31,26 +37,27 @@ R_points = [ [ cos(direction) sin(direction) ]; [-sin(direction), cos(direction)
 
 % Create new permutation tables from the provided by applying it to itself,
 % and then again
-P2 = P(P+1);
+for k = 1:size(Ps,1)
+    Ps2(k,:) = Ps(k,Ps(k,:)+1);
+end
 
 
 %%% CREATE THE MAIN FIBROSIS DEPOSIT EFFECT
 
 % Transform points according to input parameters, then call Octave2D
 P_f_points = [ R_points(1,:) / sqrt(fibre_alignment); R_points(2,:) * sqrt(fibre_alignment) ];
-P_f = Octave2DSeeded( P_f_points / feature_size, 4, roughness, P, offsets);
+P_f = Octave2D( P_f_points / feature_size, 4, roughness, Ps, offsets);
 
 
 
 %%% CREATE A LARGE-SCALE PERLIN NOISE PATTERN FOR DENSITY VARIATION
 
 % Use Octave2D with scaling of point co-ords to attain desired patch_size
-P_p = Octave2DSeeded(mesh.points' / patch_size, 3, 0.5, P2, offsets);
+P_p = Octave2D(mesh.points' / patch_size, 3, 0.5, Ps2, offsets);
 
 
 
 %%% TAKE A COMBINATION OF THESE NOISEFIELDS TO GET THE FINAL PATTERN
-%noise = (1 - patchiness) * ( fibreness * S + (1 - fibreness) * P_f ) + patchiness * P_p;
 noise = P_f + patchiness * P_p;
 
 %%% THRESHOLD THIS NOISE TO GET PRESENCE/ABSENCE OF REQUESTED DENSITY
---
+++ matchHistologyNoFibres.m
@@ -0,0 +1,146 @@
+function matchHistologyNoFibres(N_parts, histo_pattern, ellipse_mode, visualise, options)
+% This function creates a population of Perlin noise-derived patterns that
+% all fall within a sufficient degree of similarity to a target
+% histological pattern.
+%
+% INPUTS:
+%
+% N_parts:        Number of particles to use in the SMC-ABC method
+%
+% histo_pattern:  Name of histological pattern to match, options are
+%                      'interstitial', 'compact', 'diffuse', 'patchy'
+% 
+% ellipse_mode:   The set of ellipses to use for metric calculation,
+%                 options are
+%                       'standard', 'expanded', 'full'                 
+%
+% visualise:      Flag (0 or 1) for visualisation of SMC-ABC algorithm as
+%                 it progresses (visualisation function is specified)
+%
+% (options):      Options for the SMC-ABC algorithm (see SMC-ABC code for
+%                 definitions and default values)
+
+% Define the "pixel width" from the histological data
+pixel_width = 1/136;      % (mm)
+
+% Specify the histological pattern to match (if doing so), and whether to use full or reduced patterns
+reduce_patterns = 1;                % 0 - take the full images,   1 - reduce to representative sections
+
+% PARAMETERS ARE:
+% [ fibreness, fibre_separation, patchiness, feature_size, roughness, patch_size, alignment_ratio]
+
+% Define the prior for ABC-SMC. Mins and maxs are the ranges of prior,
+% scale_param selects between uniform and logarithmic prior
+params_mins = [ 0, 0.01, 0, 1, 1/2, -pi/2 ];
+params_maxs = [ 0.5, 2, 0.99, 8, 50, pi/2 ];
+scale_param = logical([ 0, 0, 0, 0, 1, 0]);
+
+
+%%% CHECK IF OPTIONS SUPPLIED
+if nargin > 4
+    options_supplied = 1;
+else
+    options_supplied = 0;
+end
+
+%%% RUN MAIN CODE
+
+% Load in the requested pattern
+presences = FibrosisImageLoader(reduce_patterns);
+switch histo_pattern
+    case 'interstitial'
+            target_pattern = presences{1};
+	case 'compact'
+            target_pattern = presences{2};
+	case 'diffuse'
+            target_pattern = presences{3};
+	case 'patchy'
+            target_pattern = presences{4};
+    otherwise
+        error('No histological pattern with that name!');
+end
+        
+% Set up the mesh according to the pattern selected
+[Ny, Nx] = size(target_pattern);
+points = buildMesh(Nx, Ny, pixel_width);
+    
+% Calculate density of pattern to be matched
+density = sum(target_pattern(:)) / numel(target_pattern);
+    
+
+% Calculate limits on the parameters after transforming scale parameters to
+% uniform
+theta_mins = params_mins;
+theta_mins(scale_param) = log(theta_mins(scale_param));
+theta_maxs = params_maxs;
+theta_maxs(scale_param) = log(theta_maxs(scale_param));
+
+% Define the function used for simulating the model (here generating a pattern) in the SMC-ABC
+f_simulate = @(params, P, offsets) createFibroPatternNoFibres(points, density, params, P, offsets);
+
+% Define the function used for calculating summary statistics in the SMC-ABC
+switch ellipse_mode
+    case 'standard'
+        f_summaries = @(pattern) ellipseMetrics(pattern, [0.1 0.5 0.8]);
+    case 'expanded'
+        f_summaries = @(pattern) ellipseMetrics(pattern, [0.2 0.6 0.9]);
+    case 'full'
+        f_summaries = @(pattern) ellipseMetrics(pattern, [0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9]);
+    otherwise
+        error('Ellipse_mode must be ''standard'', ''expanded'', or ''full''!');
+end
+
+% Define the function used for calculating discrepancies between summary statistics
+f_discrepancy = @ellipseDiscrepancy;
+
+% Define the function for visualisation
+target_ellipses = f_summaries(target_pattern);
+f_visualise = @(params, patterns, ellipses, discrepancies) visualiseResults_Fibrosis(params, patterns, ellipses, discrepancies, target_pattern, target_ellipses, theta_mins, theta_maxs);
+
+% Run the SMC-ABC
+if options_supplied
+    [part_thetas, part_vals, part_metrics, part_Ds] = performABCSMC_Fibro(N_parts, f_simulate, f_summaries, f_discrepancy, target_pattern, params_mins, params_maxs, scale_param, 1e-2, visualise, f_visualise, options);
+else
+    [part_thetas, part_vals, part_metrics, part_Ds] = performABCSMC_Fibro(N_parts, f_simulate, f_summaries, f_discrepancy, target_pattern, params_mins, params_maxs, scale_param, 1e-2, visualise, f_visualise);
+end
+
+% (TO CHANGE TO MORE BAYESIAN INTERPRETATION)
+% Save only the unique particles
+[particles.thetas, I] = unique(part_thetas, 'rows');
+particles.vals = part_vals(I);
+particles.metrics = part_metrics(I,:);
+particles.Ds = part_Ds(I);
+
+% Set up a filename according to options supplied
+filename = [histo_pattern, num2str(N_parts),'_', ellipse_mode];
+
+% Append options to filename if supplied
+if options_supplied
+        
+    % Check all fields and append to filename
+    if isfield(options,'jumping_type')
+        filename = [filename,'_jt_',options.jumping_type];
+    end
+    if isfield(options,'resample_weighting')
+        filename = [filename,'_rw_',options.resample_weighting];
+    end
+    if isfield(options,'metric_weighting')
+        filename = [filename,'_mw_',options.metric_weighting];
+    end
+    if isfield(options,'keep_fraction')
+        filename = [filename,'_kf_',num2str(options.keepfraction)];
+    end
+    if isfield(options,'max_MCMC_steps')
+        filename = [filename,'_maxMCMC_',num2str(options.max_MCMC_steps)];
+    end
+    
+end
+
+filename
+
+% Save the particles
+save([filename,'.mat'],'particles');
+
+
+end
+
